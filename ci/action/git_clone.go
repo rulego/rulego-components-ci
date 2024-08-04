c9b6b8205142eca1d70f0b1e55406284794cf938
rulego-team <rulego@outlook.com>
2024-08-04 15:19:16 +0800

    feat:add git clone node

---
+++ ci/action/git_clone.go
@@ -0,0 +1,253 @@
+package action
+
+import (
+	"crypto/tls"
+	"errors"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport"
+	"github.com/go-git/go-git/v5/plumbing/transport/client"
+	httptransport "github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	"github.com/rulego/rulego"
+	"github.com/rulego/rulego/api/types"
+	"github.com/rulego/rulego/utils/maps"
+	"github.com/rulego/rulego/utils/str"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+)
+
+func init() {
+	_ = rulego.Registry.Register(&GitCloneNode{})
+
+	//不验证https
+	var c = httptransport.NewClient(&http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	})
+	client.InstallProtocol("https", c)
+}
+
+// KeyWorkDir 工作目录
+const KeyWorkDir = "workDir"
+
+// KeyRef 分支
+const KeyRef = "ref"
+
+// KeyGitSshUrl 仓库Ssh地址
+const KeyGitSshUrl = "gitSshUrl"
+
+// KeyGitHttpUrl 仓库Http地址
+const KeyGitHttpUrl = "gitHttpUrl"
+
+// GitCloneNodeConfiguration 节点配置
+type GitCloneNodeConfiguration struct {
+	// Git 仓库 URL
+	Repository string
+	// 克隆到的本地目录
+	Directory string
+	// 分支或标签的完整引用名
+	Reference string
+	// 认证类型，可以是 "ssh-key", "username-password", 或 "token"
+	AuthType string
+	// SSH 秘钥文件路径或用户名
+	AuthUser string
+	// 密码或 token
+	AuthPassword string
+	// 代理地址
+	ProxyUrl string
+	// 代理用户名
+	ProxyUsername string
+	// 代理密码
+	ProxyPassword string
+}
+
+// GitCloneNode 实现 Git 仓库克隆
+type GitCloneNode struct {
+	// 节点配置
+	Config GitCloneNodeConfiguration
+}
+
+// Type 组件类型
+func (x *GitCloneNode) Type() string {
+	return "ci/gitClone"
+}
+
+func (x *GitCloneNode) New() types.Node {
+	return &GitCloneNode{Config: GitCloneNodeConfiguration{
+		Repository: "",
+		Directory:  "",
+		Reference:  "main",
+	}}
+}
+
+// Init 初始化
+func (x *GitCloneNode) Init(ruleConfig types.Config, configuration types.Configuration) error {
+	return maps.Map2Struct(configuration, &x.Config)
+}
+
+// OnMsg 处理消息
+func (x *GitCloneNode) OnMsg(ctx types.RuleContext, msg types.RuleMsg) {
+	ref := x.getReferenceName(msg)
+	workDir := x.getWorkDir(msg)
+	msg.Metadata.PutValue(KeyWorkDir, workDir)
+	repository := x.getRepository(msg)
+	// 检查目录是否存在
+	if _, err := os.Stat(workDir); os.IsNotExist(err) {
+		// 设置克隆选项
+		cloneOptions := &git.CloneOptions{
+			URL:      repository,
+			Progress: os.Stdout,
+		}
+		if proxy := x.getProxy(); proxy.URL != "" {
+			cloneOptions.ProxyOptions = proxy
+		}
+		// 如果指定了分支或标签，则设置为克隆特定的引用
+		if ref != "" {
+			cloneOptions.ReferenceName = plumbing.ReferenceName(ref)
+		}
+
+		// 根据 AuthType 字段的值选择认证方式
+		if auth, err := x.getAuthMethod(); err != nil {
+			ctx.TellFailure(msg, err)
+			return
+		} else {
+			cloneOptions.Auth = auth
+		}
+		// 执行克隆操作
+		if _, err := git.PlainClone(workDir, false, cloneOptions); err != nil {
+			ctx.TellFailure(msg, err)
+		} else {
+			ctx.TellSuccess(msg)
+		}
+	} else {
+		// 目录存在，执行拉取操作
+		r, err := git.PlainOpen(workDir)
+		if err != nil {
+			ctx.TellFailure(msg, err)
+			return
+		}
+		w, err := r.Worktree()
+		if err != nil {
+			ctx.TellFailure(msg, err)
+			return
+		}
+		pullOptions := &git.PullOptions{
+			//RemoteName: "origin",
+			RemoteURL: repository,
+		}
+		if proxy := x.getProxy(); proxy.URL != "" {
+			pullOptions.ProxyOptions = proxy
+		}
+		if ref != "" {
+			pullOptions.ReferenceName = plumbing.ReferenceName(ref)
+		}
+		// 根据 AuthType 字段的值选择认证方式
+		if auth, err := x.getAuthMethod(); err != nil {
+			ctx.TellFailure(msg, err)
+			return
+		} else {
+			pullOptions.Auth = auth
+		}
+		if err = w.Pull(pullOptions); err != nil {
+			if err == git.NoErrAlreadyUpToDate {
+				ctx.TellSuccess(msg)
+			} else {
+				ctx.TellFailure(msg, err)
+			}
+		} else {
+			ctx.TellSuccess(msg)
+		}
+	}
+}
+
+// Destroy 销毁
+func (x *GitCloneNode) Destroy() {
+}
+
+func (x *GitCloneNode) getAuthMethod() (transport.AuthMethod, error) {
+	// 根据 AuthType 字段的值选择认证方式
+	switch x.Config.AuthType {
+	case "ssh-key":
+		// 使用 SSH 秘钥文件
+		sshKey, err := ssh.NewPublicKeysFromFile("git", x.Config.AuthUser, x.Config.AuthPassword)
+		if err != nil {
+			return nil, err
+		}
+		return sshKey, nil
+	case "username-password":
+		// 使用用户名和密码
+		auth := &httptransport.BasicAuth{
+			Username: x.Config.AuthUser,
+			Password: x.Config.AuthPassword,
+		}
+		return auth, nil
+	case "token":
+		// 使用 token
+		auth := &httptransport.BasicAuth{
+			Username: x.Config.AuthUser, // 注意：GitHub 个人访问令牌使用时，用户名可以是任意字符串
+			Password: x.Config.AuthPassword,
+		}
+		return auth, nil
+	}
+	return nil, errors.New("not authType=" + x.Config.AuthType)
+}
+func (x *GitCloneNode) getWorkDir(msg types.RuleMsg) string {
+	workDir := x.Config.Directory
+	if workDir == "" {
+		workDir = msg.Metadata.GetValue(KeyWorkDir)
+	} else {
+		workDir = str.SprintfDict(workDir, msg.Metadata.Values())
+	}
+	workDir = path.Join(workDir, x.getRepoName(x.getRepository(msg)))
+	return workDir
+}
+
+func (x *GitCloneNode) getReferenceName(msg types.RuleMsg) string {
+	ref := x.Config.Reference
+	if ref == "" {
+		ref = msg.Metadata.GetValue(KeyRef)
+	} else {
+		ref = str.SprintfDict(ref, msg.Metadata.Values())
+	}
+	return ref
+}
+
+func (x *GitCloneNode) getRepository(msg types.RuleMsg) string {
+	repository := x.Config.Repository
+	if repository == "" {
+		if x.Config.AuthType == "ssh-key" {
+			repository = msg.Metadata.GetValue(KeyGitSshUrl)
+		} else {
+			repository = msg.Metadata.GetValue(KeyGitHttpUrl)
+		}
+	} else {
+		repository = str.SprintfDict(repository, msg.Metadata.Values())
+	}
+	return repository
+}
+
+// GetRepoName 从 Git 仓库 URL 中提取仓库名称
+func (x *GitCloneNode) getRepoName(repoURL string) string {
+	// 分割 URL 来获取仓库名称部分
+	parts := strings.Split(repoURL, "/")
+	// 仓库名称是 URL 的最后一部分
+	repoName := parts[len(parts)-1]
+	// 移除 ".git" 后缀
+	repoName = strings.TrimSuffix(repoName, ".git")
+	return repoName
+}
+
+func (x *GitCloneNode) getProxy() transport.ProxyOptions {
+	if x.Config.ProxyUrl != "" {
+		return transport.ProxyOptions{
+			URL:      x.Config.ProxyUrl,
+			Username: x.Config.ProxyUsername,
+			Password: x.Config.ProxyPassword,
+		}
+	}
+	return transport.ProxyOptions{}
+}
--- go.mod
+++ go.mod
@@ -1,27 +1,49 @@
 module github.com/rulego/rulego-components-ci
 
-go 1.18
+go 1.19
+
 require (
-	github.com/rulego/rulego v0.20.0
+	github.com/go-git/go-git/v5 v5.11.0
+	github.com/rulego/rulego v0.22.1-0.20240729093613-233652e7cd8c
 )
 
 require (
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20230828082145-3c4c8a2d2371 // indirect
+	github.com/cloudflare/circl v1.3.3 // indirect
+	github.com/cyphar/filepath-securejoin v0.2.4 // indirect
 	github.com/dlclark/regexp2 v1.7.0 // indirect
 	github.com/dop251/goja v0.0.0-20231024180952-594410467bc6 // indirect
 	github.com/eclipse/paho.mqtt.golang v1.4.2 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/expr-lang/expr v1.16.0 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.5.0 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/gofrs/uuid/v5 v5.0.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/pprof v0.0.0-20230207041349-798e818bf904 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pjbgf/sha1cd v0.3.0 // indirect
+	github.com/robfig/cron/v3 v3.0.0 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/skeema/knownhosts v1.2.1 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
 
 //replace github.com/rulego/rulego => ../rulego
